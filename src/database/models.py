74d357859824e40b18caab487172b9c5b2f0eabd
CarlosIRB <ramirezbelloci@gmail.com>
2024-04-11 12:18:59 -0600

    Formateo

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,3 @@
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
@@ -6,26 +5,28 @@ from sqlalchemy import func, Enum
 
 db = SQLAlchemy()
 
-asignacion_rol_usuario = db.Table('asignaciones_rol_usuario',
-    db.Column('idUsuario', db.Integer, db.ForeignKey('usuarios.id')),
-    db.Column('idRol', db.Integer, db.ForeignKey('roles.id'))
+asignacion_rol_usuario = db.Table(
+    "asignaciones_rol_usuario",
+    db.Column("idUsuario", db.Integer, db.ForeignKey("usuarios.id")),
+    db.Column("idRol", db.Integer, db.ForeignKey("roles.id")),
 )
 
+
 class InsumosReceta(db.Model):
-    __tablename__ = 'insumos_receta'
-    idReceta = db.Column(db.Integer, db.ForeignKey('recetas.id'), primary_key=True)
-    idInsumo = db.Column(db.Integer, db.ForeignKey('insumos.id'), primary_key=True)
+    __tablename__ = "insumos_receta"
+    idReceta = db.Column(db.Integer, db.ForeignKey("recetas.id"), primary_key=True)
+    idInsumo = db.Column(db.Integer, db.ForeignKey("insumos.id"), primary_key=True)
     cantidad = db.Column(db.Float, nullable=False)
-    
-    receta = db.relationship('Receta', backref='insumos')
-    insumo = db.relationship('Insumo', backref='recetas')
-    
+
+    receta = db.relationship("Receta", backref="insumos")
+    insumo = db.relationship("Insumo", backref="recetas")
+
     def __str__(self):
         return f"{self.insumo.nombre} en {self.receta.nombre}"
-    
-    
+
+
 class Receta(db.Model):
-    __tablename__ = 'recetas'
+    __tablename__ = "recetas"
 
     id = db.Column(db.Integer, primary_key=True)
     peso_estimado = db.Column(db.Float, nullable=False)
@@ -35,44 +36,46 @@ class Receta(db.Model):
     nombre = db.Column(db.String(50), nullable=False, unique=True)
     imagen = db.Column(db.String(2550), nullable=False)
     estatus = db.Column(db.Boolean, nullable=False, default=True)
-    
+
     # Método para obtener solo el nombre
     def __str__(self):
         return self.nombre
 
-    
-    
+
 class Insumo(db.Model):
-    __tablename__ = 'insumos'
-    
+    __tablename__ = "insumos"
+
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(45), nullable=False)
     descripcion = db.Column(db.String(45), nullable=False)
-    unidad_medida = db.Column(Enum('Kilos','Litros'), nullable=False)
+    unidad_medida = db.Column(Enum("Kilos", "Litros"), nullable=False)
     cantidad_maxima = db.Column(db.Float, nullable=False)
     cantidad_minima = db.Column(db.Float, nullable=False)
     merma = db.Column(db.Float, nullable=False)
     estatus = db.Column(db.Boolean, nullable=False, default=True)
-    
+
     def __str__(self):
         return self.nombre
 
+
 class Rol(db.Model):
-    __tablename__="roles"
-    
+    __tablename__ = "roles"
+
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(45), unique=True, nullable=False)
     descripcion = db.Column(db.String(255))
-    usuarios = db.relationship('Usuario', secondary=asignacion_rol_usuario, back_populates='roles')
-    
+    usuarios = db.relationship(
+        "Usuario", secondary=asignacion_rol_usuario, back_populates="roles"
+    )
+
     # Método para obtener solo el nombre
     def __str__(self):
         return self.nombre
-    
+
 
 class Usuario(UserMixin, db.Model):
-    __tablename__="usuarios"
-    
+    __tablename__ = "usuarios"
+
     id = db.Column(db.Integer, primary_key=True)
     correo = db.Column(db.String(255), unique=True, nullable=False)
     nombre = db.Column(db.String(255), unique=True, nullable=False)
@@ -80,37 +83,44 @@ class Usuario(UserMixin, db.Model):
     token = db.Column(db.String(255), nullable=True)
     last_login_at = db.Column(db.DateTime)
     current_login_at = db.Column(db.DateTime)
-    #last_login_ip = db.Column(db.String(100))
-    #current_login_ip = db.Column(db.String(100))
-    #login_count = db.Column(db.Integer)
+    # last_login_ip = db.Column(db.String(100))
+    # current_login_ip = db.Column(db.String(100))
+    # login_count = db.Column(db.Integer)
     is_active = db.Column(db.Boolean(), default=True)
     is_authenticated = db.Column(db.Boolean(), default=True)
     is_anonymous = db.Column(db.Boolean(), default=False)
     estatus = db.Column(db.Boolean(), nullable=False, default=True)
-    #fs_uniquifier = db.Column(db.String(64), unique=True, nullable=False)
+    # fs_uniquifier = db.Column(db.String(64), unique=True, nullable=False)
     confirmed_at = db.Column(db.DateTime)
-    
-    roles = db.relationship('Rol', secondary=asignacion_rol_usuario, back_populates='usuarios')
-    
-    
+
+    roles = db.relationship(
+        "Rol", secondary=asignacion_rol_usuario, back_populates="usuarios"
+    )
+
     # Método para obtener solo el nombre
     def __str__(self):
         return self.nombre
-    
-    #metodo para flask-login
+
+    # metodo para flask-login
     def get_id(self):
         return str(self.id)
-    
-    #metodo para acelerar la validacion de roles
+
+    # metodo para acelerar la validacion de roles
     def has_role(self, role):
-        return bool(Rol.query.join(asignacion_rol_usuario).join(Usuario).filter(Rol.nombre == role, Usuario.id == self.id).first())
-    
+        return bool(
+            Rol.query.join(asignacion_rol_usuario)
+            .join(Usuario)
+            .filter(Rol.nombre == role, Usuario.id == self.id)
+            .first()
+        )
+
     def get_roles(self):
         return [role.nombre for role in self.roles]
-    
+
+
 class Proveedor(db.Model):
-    __tablename__ = 'proveedores'
-    
+    __tablename__ = "proveedores"
+
     id = db.Column(db.Integer, primary_key=True)
     empresa = db.Column(db.String(45), nullable=False)
     direccion = db.Column(db.String(45), nullable=False)
@@ -118,94 +128,119 @@ class Proveedor(db.Model):
     contacto = db.Column(db.String(45), nullable=True)
     estatus = db.Column(db.Boolean, nullable=False, default=True)
 
+
 class Compra(db.Model):
-    __tablename__ = 'compras'
-    
+    __tablename__ = "compras"
+
     id = db.Column(db.Integer, primary_key=True)
     pago_proveedor = db.Column(db.Float, nullable=False)
-    
-    idUsuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+
+    idUsuario = db.Column(db.Integer, db.ForeignKey("usuarios.id"))
     idTransaccionCaja = db.Column(db.Integer, primary_key=True)
-    idProveedores = db.Column(db.Integer, db.ForeignKey('proveedores.id'), primary_key=True)
-    
+    idProveedores = db.Column(
+        db.Integer, db.ForeignKey("proveedores.id"), primary_key=True
+    )
+
+
 class Venta(db.Model):
-    __tablename__ = 'ventas'
-    
+    __tablename__ = "ventas"
+
     id = db.Column(db.Integer, primary_key=True)
     fecha_venta = db.Column(db.DateTime, nullable=False)
     total_venta = db.Column(db.Float, nullable=False)
-    
-    idUsuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
-    idProveedores = db.Column(db.Integer, db.ForeignKey('proveedores.id'), primary_key=True)
+
+    idUsuario = db.Column(db.Integer, db.ForeignKey("usuarios.id"))
+    idProveedores = db.Column(
+        db.Integer, db.ForeignKey("proveedores.id"), primary_key=True
+    )
+
 
 class CorteCaja(db.Model):
-    __tablename__ = 'cortes_caja'
-    
+    __tablename__ = "cortes_caja"
+
     id = db.Column(db.Integer, primary_key=True)
     monto_final = db.Column(db.Float)
     monto_inicial = db.Column(db.Float)
     fecha_corte = db.Column(db.Date)
 
+
 class DetalleVenta(db.Model):
-    __tablename__ = 'detalles_venta'
-    
+    __tablename__ = "detalles_venta"
+
     precio = db.Column(db.Float, nullable=False)
     cantidad = db.Column(db.Integer)
-    
-    idVenta = db.Column(db.Integer, db.ForeignKey('ventas.id'), primary_key=True)
-    idStock = db.Column(db.Integer, db.ForeignKey('lotes_galletas.id'), primary_key=True)
+
+    idVenta = db.Column(db.Integer, db.ForeignKey("ventas.id"), primary_key=True)
+    idStock = db.Column(
+        db.Integer, db.ForeignKey("lotes_galletas.id"), primary_key=True
+    )
+
 
 class LogAccion(db.Model):
-    __tablename__ = 'logs_acciones'
-    
+    __tablename__ = "logs_acciones"
+
     id = db.Column(db.Integer, primary_key=True)
     fecha = db.Column(db.DateTime, nullable=False)
     modulo = db.Column(db.String(45), nullable=False)
     detalles = db.Column(db.String(200), nullable=False)
-    
-    idUsuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+
+    idUsuario = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=False)
+
 
 class LogLogin(db.Model):
-    __tablename__ = 'logs_login'
-    
+    __tablename__ = "logs_login"
+
     id = db.Column(db.Integer, primary_key=True)
     fecha = db.Column(db.DateTime, nullable=False)
     exito = db.Column(db.Boolean, nullable=False)
-    
-    idUsuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=True)
+
+    idUsuario = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=True)
+
 
 class LoteGalleta(db.Model):
-    __tablename__ = 'lotes_galletas'
-    
+    __tablename__ = "lotes_galletas"
+
     id = db.Column(db.Integer, primary_key=True)
     fecha_entrada = db.Column(db.Date)
     cantidad = db.Column(db.Integer, nullable=False)
     merma = db.Column(db.Float, nullable=False, default=0)
     tipo_venta = db.Column(db.Integer, nullable=False)
-    
-    idProduccion = db.Column(db.Integer, db.ForeignKey('solicitudes_produccion.id'), nullable=False)
-    idReceta = db.Column(db.Integer, db.ForeignKey('recetas.id'), nullable=False)
-    idUsuarios = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
-    
-    receta = db.relationship('Receta',foreign_keys=[idReceta], backref=db.backref('lotes_receta', lazy='dynamic'))
-    usuario = db.relationship('Usuario',foreign_keys=[idUsuarios], backref=db.backref('lotes_usuario', lazy='dynamic'))
+
+    idProduccion = db.Column(
+        db.Integer, db.ForeignKey("solicitudes_produccion.id"), nullable=False
+    )
+    idReceta = db.Column(db.Integer, db.ForeignKey("recetas.id"), nullable=False)
+    idUsuarios = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=False)
+
+    receta = db.relationship(
+        "Receta",
+        foreign_keys=[idReceta],
+        backref=db.backref("lotes_receta", lazy="dynamic"),
+    )
+    usuario = db.relationship(
+        "Usuario",
+        foreign_keys=[idUsuarios],
+        backref=db.backref("lotes_usuario", lazy="dynamic"),
+    )
+
 
 class LoteInsumo(db.Model):
-    __tablename__ = 'lotes_insumo'
-    
+    __tablename__ = "lotes_insumo"
+
     id = db.Column(db.Integer, primary_key=True)
     fecha_caducidad = db.Column(db.Date, nullable=False)
     cantidad = db.Column(db.Float, nullable=False)
     fecha_compra = db.Column(db.Date, nullable=False)
     precio_unidad = db.Column(db.Float, nullable=False)
     merma = db.Column(db.Float, nullable=False, default=0)
-    
-    idInsumo = db.Column(db.Integer, db.ForeignKey('insumos.id'), primary_key=True)
-    idCompra = db.Column(db.Integer, db.ForeignKey('compras.id'), primary_key=True)
+
+    idInsumo = db.Column(db.Integer, db.ForeignKey("insumos.id"), primary_key=True)
+    idCompra = db.Column(db.Integer, db.ForeignKey("compras.id"), primary_key=True)
+
 
 class SolicitudProduccion(db.Model):
-    __tablename__ = 'solicitudes_produccion'
-    
+    __tablename__ = "solicitudes_produccion"
+
     id = db.Column(db.Integer, primary_key=True)
 
     fecha_produccion = db.Column(db.Date)
@@ -217,19 +252,32 @@ class SolicitudProduccion(db.Model):
 
     fecha_solicitud = db.Column(db.DateTime, default=datetime.now)
     fecha_produccion = db.Column(db.Date)
-    
+
     posicion = db.Column(db.Integer, default=1)
-    idReceta = db.Column(db.Integer, db.ForeignKey('recetas.id'), nullable=False)
+    idReceta = db.Column(db.Integer, db.ForeignKey("recetas.id"), nullable=False)
 
-    idUsuarioSolicitud = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
-    idUsuarioProduccion = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
-    
-    receta = db.relationship('Receta', backref=db.backref('solicitudes_produccion', lazy='dynamic'))
-    
-    usuarioSolicitud = db.relationship('Usuario', foreign_keys=[idUsuarioSolicitud], backref=db.backref('solicitudes_produccion_vendedor', lazy='dynamic'))
-    usuarioProduccion = db.relationship('Usuario', foreign_keys=[idUsuarioProduccion], backref=db.backref('solicitudes_produccion_cocinero', lazy='dynamic'))
+    idUsuarioSolicitud = db.Column(
+        db.Integer, db.ForeignKey("usuarios.id"), nullable=False
+    )
+    idUsuarioProduccion = db.Column(
+        db.Integer, db.ForeignKey("usuarios.id"), nullable=False
+    )
 
-    
+    receta = db.relationship(
+        "Receta", backref=db.backref("solicitudes_produccion", lazy="dynamic")
+    )
+
+    usuarioSolicitud = db.relationship(
+        "Usuario",
+        foreign_keys=[idUsuarioSolicitud],
+        backref=db.backref("solicitudes_produccion_vendedor", lazy="dynamic"),
+    )
+    usuarioProduccion = db.relationship(
+        "Usuario",
+        foreign_keys=[idUsuarioProduccion],
+        backref=db.backref("solicitudes_produccion_cocinero", lazy="dynamic"),
+    )
+
     @staticmethod
     def get_next_position():
         # Obtiene la siguiente posición disponible
@@ -238,17 +286,18 @@ class SolicitudProduccion(db.Model):
 
     def __init__(self, *args, **kwargs):
         super(SolicitudProduccion, self).__init__(*args, **kwargs)
-        if self.posicion is None:  # Si no se proporciona una posición, asigna la siguiente disponible
+        if (
+            self.posicion is None
+        ):  # Si no se proporciona una posición, asigna la siguiente disponible
             self.posicion = self.get_next_position()
-    
 
 
 class TransaccionCaja(db.Model):
-    __tablename__ = 'transacciones_caja'
-    
+    __tablename__ = "transacciones_caja"
+
     id = db.Column(db.Integer, primary_key=True)
     monto_egreso = db.Column(db.Float)
     monto_ingreso = db.Column(db.Float)
     fecha_transaccion = db.Column(db.Date)
-    
-    idCorteCaja = db.Column(db.Integer, db.ForeignKey('cortes_caja.id'), nullable=False)
+
+    idCorteCaja = db.Column(db.Integer, db.ForeignKey("cortes_caja.id"), nullable=False)
--- src/routes/venta.py
+++ src/routes/venta.py
@@ -1,11 +1,32 @@
-from flask import Blueprint, Flask, request, render_template, Response, redirect, url_for
+from flask import (
+    Blueprint,
+    Flask,
+    request,
+    render_template,
+    Response,
+    redirect,
+    url_for,
+)
 from flask_login import login_required, current_user
 
 from flask_wtf.csrf import CSRFProtect
 from flask import g
 from flask import flash, jsonify
 from configu.config import DevelopmentConfig
-from database.models import db, Usuario, Rol, asignacion_rol_usuario, Insumo, Receta, SolicitudProduccion, Venta, DetalleVenta, Compra, Proveedor, LoteGalleta
+from database.models import (
+    db,
+    Usuario,
+    Rol,
+    asignacion_rol_usuario,
+    Insumo,
+    Receta,
+    SolicitudProduccion,
+    Venta,
+    DetalleVenta,
+    Compra,
+    Proveedor,
+    LoteGalleta,
+)
 
 from forms import forms
 from sqlalchemy.orm import joinedload
@@ -13,21 +34,23 @@ from sqlalchemy import desc, extract, text
 from datetime import datetime, timedelta
 from routes.auth import requires_role
 
-venta = Blueprint('venta', __name__, url_prefix="/venta")
+venta = Blueprint("venta", __name__, url_prefix="/venta")
 
 
-@venta.route('/compras')
-@requires_role('vendedor')
+@venta.route("/compras")
+@requires_role("vendedor")
 def compras():
-    return render_template('modulos/venta/compras.html')
+    return render_template("modulos/venta/compras.html")
 
-@venta.route('/punto_venta')
-@requires_role('vendedor')
+
+@venta.route("/punto_venta")
+@requires_role("vendedor")
 def punto_venta():
-    return render_template('modulos/venta/punto-venta.html')
+    return render_template("modulos/venta/punto-venta.html")
 
-@venta.route('/solicitud_produccion')
-@requires_role('vendedor')
+
+@venta.route("/solicitud_produccion")
+@requires_role("vendedor")
 def solicitud_produccion():
     solicitudes = SolicitudProduccion.query.options(
         # Asume que 'receta' es el nombre de la relación en SolicitudProduccion
@@ -35,93 +58,121 @@ def solicitud_produccion():
         # Asume que 'usuarioSolicitud' es el nombre de la relación en SolicitudProduccion
         joinedload(SolicitudProduccion.usuarioSolicitud),
         # Asume que 'usuarioProduccion' es el nombre de la relación en SolicitudProduccion
-        joinedload(SolicitudProduccion.usuarioProduccion)
+        joinedload(SolicitudProduccion.usuarioProduccion),
     ).all()
 
-    return render_template('modulos/venta/solicitudes-produccion.html', solicitudes=solicitudes)
+    return render_template(
+        "modulos/venta/solicitudes-produccion.html", solicitudes=solicitudes
+    )
 
 
-@venta.route('/solicitud_produccion/crear', methods=["GET", "POST"])
-@requires_role('vendedor')
+@venta.route("/solicitud_produccion/crear", methods=["GET", "POST"])
+@requires_role("vendedor")
 def solicitud_produccion_nuevo():
     form = forms.SolicitudProduccionForm(request.form)
     form.receta.choices = [(receta.id, receta.nombre) for receta in Receta.query.all()]
     images = [(receta.id, receta.imagen) for receta in Receta.query.all()]
-    
+
     if request.method == "GET":
-        return render_template('modulos/venta/solicitudesProduccion/create.html', form=form, images=images, nuevo=True)
+        return render_template(
+            "modulos/venta/solicitudesProduccion/create.html",
+            form=form,
+            images=images,
+            nuevo=True,
+        )
     else:
         if form.validate():
-            usuario_cocinero=0
-            rol_cocinero = Rol.query.filter_by(nombre='cocinero').first()
+            usuario_cocinero = 0
+            rol_cocinero = Rol.query.filter_by(nombre="cocinero").first()
             if rol_cocinero is not None:
                 # Luego, filtra los usuarios que tienen el rol 'cocinero' y ordena por 'is_active'
-                usuario_cocinero = Usuario.query.join(asignacion_rol_usuario).join(Rol).filter(Rol.id == rol_cocinero.id).order_by(desc('is_active')).first()
+                usuario_cocinero = (
+                    Usuario.query.join(asignacion_rol_usuario)
+                    .join(Rol)
+                    .filter(Rol.id == rol_cocinero.id)
+                    .order_by(desc("is_active"))
+                    .first()
+                )
 
                 # Si no se encuentra un usuario cocinero con is_active, intenta obtener el usuario con el last_login_at más reciente
                 if usuario_cocinero is None:
-                    usuario_cocinero = Usuario.query.join(asignacion_rol_usuario).join(Rol).filter(Rol.id == rol_cocinero.id).order_by(desc('last_login_at')).first()
+                    usuario_cocinero = (
+                        Usuario.query.join(asignacion_rol_usuario)
+                        .join(Rol)
+                        .filter(Rol.id == rol_cocinero.id)
+                        .order_by(desc("last_login_at"))
+                        .first()
+                    )
 
                 if usuario_cocinero is not None:
                     usuario_cocinero = usuario_cocinero.id
                 else:
-                        # Maneja el caso en que no se encuentre ningún usuario cocinero
+                    # Maneja el caso en que no se encuentre ningún usuario cocinero
                     usuario_cocinero = current_user.id
-            
+
             solicitud = SolicitudProduccion(
                 idReceta=form.receta.data,
                 idUsuarioSolicitud=current_user.id,
                 idUsuarioProduccion=usuario_cocinero,
                 tandas=form.tandas.data,
-                estatus=1
+                estatus=1,
             )
 
             db.session.add(solicitud)
             db.session.commit()
 
-            flash('Solicitud de producción creada correctamente', 'success')
+            flash("Solicitud de producción creada correctamente", "success")
 
-            return redirect(url_for('venta.solicitud_produccion'))
+            return redirect(url_for("venta.solicitud_produccion"))
         else:
-            return render_template('modulos/venta/solicitudesProduccion/create.html', form=form, images= images, nuevo=True)
+            return render_template(
+                "modulos/venta/solicitudesProduccion/create.html",
+                form=form,
+                images=images,
+                nuevo=True,
+            )
 
-@venta.route('/solicitud_produccion/edit/<int:id>', methods=['GET', 'POST'])
-@requires_role('vendedor')
+
+@venta.route("/solicitud_produccion/edit/<int:id>", methods=["GET", "POST"])
+@requires_role("vendedor")
 def edit_solicitud_produccion(id):
     solicitud = SolicitudProduccion.query.get(id)
-    
+
     form = forms.ModificarSolicitudProduccionForm(request.form)
 
-    if request.method == 'POST' and form.validate():
+    if request.method == "POST" and form.validate():
         solicitud = SolicitudProduccion.query.get(id)
         solicitud.tandas = form.tandas.data
         solicitud.fecha_solicitud = form.fecha_solicitud.data
         db.session.commit()
-        
-        flash('Solicitud de producción modificada correctamente', 'success')
-        
-        return redirect(url_for('venta.solicitud_produccion'))
 
-    return render_template('modulos/venta/solicitudesProduccion/edit.html', form=form, solicitud = solicitud)
+        flash("Solicitud de producción modificada correctamente", "success")
+
+        return redirect(url_for("venta.solicitud_produccion"))
+
+    return render_template(
+        "modulos/venta/solicitudesProduccion/edit.html", form=form, solicitud=solicitud
+    )
 
 
-@venta.route('/solicitud_produccion/delete', methods=['POST'])
-@requires_role('vendedor')
+@venta.route("/solicitud_produccion/delete", methods=["POST"])
+@requires_role("vendedor")
 def delete_solicitud_produccion():
-    id = request.form.get('id')
+    id = request.form.get("id")
     solicitud = SolicitudProduccion.query.get_or_404(id)
     db.session.delete(solicitud)
     db.session.commit()
-    flash('Solicitud de producción eliminada correctamente', 'success')
-    return redirect(url_for('venta.solicitud_produccion'))
+    flash("Solicitud de producción eliminada correctamente", "success")
+    return redirect(url_for("venta.solicitud_produccion"))
 
 
-@venta.route('/almacen/galletas', methods=['GET', 'POST'])
+@venta.route("/almacen/galletas", methods=["GET", "POST"])
 def lotes_galletas():
     form = forms.BusquedaLoteGalletaForm(request.form)
 
-    form.receta.choices = [(0, 'Todas las recetas')] + \
-        [(receta.id, receta.nombre) for receta in Receta.query.all()]
+    form.receta.choices = [(0, "Todas las recetas")] + [
+        (receta.id, receta.nombre) for receta in Receta.query.all()
+    ]
 
     if request.method == "POST" and form.validate():
         fecha_inicio = form.fecha_inicio.data
@@ -129,36 +180,48 @@ def lotes_galletas():
         receta = form.receta.data
         lotes = []
 
-        base_query = db.session.query(LoteGalleta, Receta.nombre, Usuario.nombre).join(
-            Receta, LoteGalleta.idReceta == Receta.id).join(Usuario, LoteGalleta.idUsuarios == Usuario.id)
+        base_query = (
+            db.session.query(LoteGalleta, Receta.nombre, Usuario.nombre)
+            .join(Receta, LoteGalleta.idReceta == Receta.id)
+            .join(Usuario, LoteGalleta.idUsuarios == Usuario.id)
+        )
         print(fecha_inicio, fecha_fin, receta)
         if receta == 0:
             lotes = base_query.filter(
                 LoteGalleta.fecha_entrada >= fecha_inicio,
                 LoteGalleta.fecha_entrada <= fecha_fin,
-                LoteGalleta.cantidad > 0
+                LoteGalleta.cantidad > 0,
             ).all()
         else:
             lotes = base_query.filter(
                 LoteGalleta.fecha_entrada >= fecha_inicio,
                 LoteGalleta.fecha_entrada <= fecha_fin,
                 LoteGalleta.idReceta == receta,
-                LoteGalleta.cantidad > 0
+                LoteGalleta.cantidad > 0,
             ).all()
 
-        return render_template('modulos/venta/galletas.html', form=form, lotes=lotes, lista=True)
+        return render_template(
+            "modulos/venta/galletas.html", form=form, lotes=lotes, lista=True
+        )
 
-    lotes = db.session.query(LoteGalleta, Receta.nombre, Usuario.nombre).join(Receta, LoteGalleta.idReceta == Receta.id).join(
-        Usuario, LoteGalleta.idUsuarios == Usuario.id).filter(LoteGalleta.cantidad > 0).all()
+    lotes = (
+        db.session.query(LoteGalleta, Receta.nombre, Usuario.nombre)
+        .join(Receta, LoteGalleta.idReceta == Receta.id)
+        .join(Usuario, LoteGalleta.idUsuarios == Usuario.id)
+        .filter(LoteGalleta.cantidad > 0)
+        .all()
+    )
 
-    return render_template('modulos/venta/galletas.html', form=form, lotes=lotes, lista=True)
+    return render_template(
+        "modulos/venta/galletas.html", form=form, lotes=lotes, lista=True
+    )
 
 
-@venta.route('/merma/galletas', methods=['GET', 'POST'])
+@venta.route("/merma/galletas", methods=["GET", "POST"])
 def merma_galletas():
     form = forms.MermaGalletaForm(request.form)
     if form.validate():
         tipo_medida = form.tipo_medida.data
         cantidad = form.cantidad.data
 
-    return redirect(url_for('venta.lotes_galletas'))
+    return redirect(url_for("venta.lotes_galletas"))
