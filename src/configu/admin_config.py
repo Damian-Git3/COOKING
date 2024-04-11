5fbb69a3d5bca45119924591fa5188cdc193d841
Fernando <auxiliarinnovacion4_lem@castores.com.mx>
2024-04-11 12:14:48 -0600

    Formateo de Codigo

--- src/configu/admin_config.py
+++ src/configu/admin_config.py
@@ -12,6 +12,7 @@ from flask import redirect, url_for, request
 
 import os
 
+
 class AdminModelView(ModelView):
     def is_accessible(self):
         """
@@ -20,7 +21,7 @@ class AdminModelView(ModelView):
         """
         if not current_user.is_authenticated:
             return False
-        if not current_user.has_rol('admin'):
+        if not current_user.has_rol("admin"):
             return False
         return True
 
@@ -29,8 +30,9 @@ class AdminModelView(ModelView):
         Redirige al usuario a la página de inicio de sesión si intenta acceder a una vista
         a la que no tiene acceso.
         """
-        return redirect(url_for('auth.login', next=request.url))
-    
+        return redirect(url_for("auth.login", next=request.url))
+
+
 class AdminIndexView(AdminIndexView):
     def is_accessible(self):
         """
@@ -39,7 +41,7 @@ class AdminIndexView(AdminIndexView):
         """
         if not current_user.is_authenticated:
             return False
-        if not current_user.has_rol('admin'):
+        if not current_user.has_rol("admin"):
             return False
         return True
 
@@ -48,15 +50,16 @@ class AdminIndexView(AdminIndexView):
         Redirige al usuario a la página de inicio de sesión si intenta acceder a una vista
         a la que no tiene acceso.
         """
-        return redirect(url_for('auth.login', next=request.url))
+        return redirect(url_for("auth.login", next=request.url))
 
 
 def setup_admin(app, db):
-    
-    admin = Admin(app, index_view=AdminIndexView(
-        menu_icon_type='fa-solid',
-        menu_icon_value='fa-home'
-    ), template_mode='bootstrap4')
+
+    admin = Admin(
+        app,
+        index_view=AdminIndexView(menu_icon_type="fa-solid", menu_icon_value="fa-home"),
+        template_mode="bootstrap4",
+    )
 
     # Clase base para modelos configuration
     class BaseModelConfiguration(AdminModelView):
@@ -64,30 +67,40 @@ def setup_admin(app, db):
 
     # Clase de vista personalizada para Usuario
     class UsuarioView(BaseModelConfiguration):
-        form_columns = ['nombre', 'correo', 'estatus', 'roles']
-        column_list = ['nombre', 'correo', 'estatus', 'roles']
-        column_searchable_list = ('nombre', 'correo')
+        form_columns = ["nombre", "correo", "estatus", "roles"]
+        column_list = ["nombre", "correo", "estatus", "roles"]
+        column_searchable_list = ("nombre", "correo")
 
         def on_model_change(self, form, Usuario, is_created):
             if is_created:
-                Usuario.contrasenia = generate_password_hash('1234')
-                flash(
-                    f"La contraseña automatica para {Usuario.nombre} es 1234")
+                Usuario.contrasenia = generate_password_hash("1234")
+                flash(f"La contraseña automatica para {Usuario.nombre} es 1234")
 
     class RecetaView(BaseModelConfiguration):
-        form_columns = ['nombre', 'descripcion', 'piezas', 'utilidad', 'peso_estimado', 'imagen']
-        column_list = ['nombre', 'descripcion', 'piezas', 'utilidad', 'peso_estimado']
-        
-        inline_models = ((
-            InsumosReceta,
-            {
-                'form_columns': ('cantidad', 'insumo', 'idReceta', 'idInsumo'),
-                'form_label': 'Insumos'
-            }
-        ),)
+        form_columns = [
+            "nombre",
+            "descripcion",
+            "piezas",
+            "utilidad",
+            "peso_estimado",
+            "imagen",
+        ]
+        column_list = ["nombre", "descripcion", "piezas", "utilidad", "peso_estimado"]
+
+        inline_models = (
+            (
+                InsumosReceta,
+                {
+                    "form_columns": ("cantidad", "insumo", "idReceta", "idInsumo"),
+                    "form_label": "Insumos",
+                },
+            ),
+        )
 
         column_formatters = {
-            'imagen': lambda v, c, m, p: Markup(f'<img src="{url_for("static", filename="img/cookies/" + m.imagen)}" style="max-width:300px; max-height:300px;">')
+            "imagen": lambda v, c, m, p: Markup(
+                f'<img src="{url_for("static", filename="img/cookies/" + m.imagen)}" style="max-width:300px; max-height:300px;">'
+            )
         }
 
         def delete_model(self, model):
@@ -97,21 +110,41 @@ def setup_admin(app, db):
                 # Llamar al método delete_model de la superclase para eliminar la receta
                 return super(RecetaView, self).delete_model(model)
             except Exception as e:
-                flash('Error al eliminar la receta y los registros relacionados: {}'.format(
-                    str(e)), 'error')
+                flash(
+                    "Error al eliminar la receta y los registros relacionados: {}".format(
+                        str(e)
+                    ),
+                    "error",
+                )
                 return False
-    
+
     class InsumoView(BaseModelConfiguration):
-        form_columns = ['nombre', 'descripcion', 'unidad_medida', 'cantidad_maxima', 'cantidad_minima', 'merma']
-        column_list = ['nombre', 'descripcion', 'unidad_medida', 'cantidad_maxima', 'cantidad_minima', 'merma']
-        
-        inline_models = ((
-            InsumosReceta,
-            {
-                'form_columns': ('cantidad', 'receta', 'idReceta', 'idInsumo'),
-                'form_label': 'Recetas Asociadas'
-            }
-        ),)
+        form_columns = [
+            "nombre",
+            "descripcion",
+            "unidad_medida",
+            "cantidad_maxima",
+            "cantidad_minima",
+            "merma",
+        ]
+        column_list = [
+            "nombre",
+            "descripcion",
+            "unidad_medida",
+            "cantidad_maxima",
+            "cantidad_minima",
+            "merma",
+        ]
+
+        inline_models = (
+            (
+                InsumosReceta,
+                {
+                    "form_columns": ("cantidad", "receta", "idReceta", "idInsumo"),
+                    "form_label": "Recetas Asociadas",
+                },
+            ),
+        )
 
         column_formatters = {
             # Aquí puedes agregar formatters personalizados si es necesario
@@ -124,34 +157,58 @@ def setup_admin(app, db):
                 # Llamar al método delete_model de la superclase para eliminar el insumo
                 return super(InsumoView, self).delete_model(model)
             except Exception as e:
-                flash('Error al eliminar el insumo y los registros relacionados: {}'.format(
-                    str(e)), 'error')
+                flash(
+                    "Error al eliminar el insumo y los registros relacionados: {}".format(
+                        str(e)
+                    ),
+                    "error",
+                )
                 return False
 
     # Clase de vista personalizada para Rol
     class RolView(BaseModelConfiguration):
         can_create = True
         can_delete = False
-        form_colums = ['descripcion', 'usuarios']
-        column_list = ['nombre', 'descripcion', 'usuarios']
+        form_colums = ["descripcion", "usuarios"]
+        column_list = ["nombre", "descripcion", "usuarios"]
 
     babel = Babel(app)
 
     # Modificiar idioma de flask_admin
     def get_locale():
-        return 'es'
+        return "es"
+
     babel.init_app(app, locale_selector=get_locale)
 
     # Agregar módulos a Flask_Admin
-    admin.add_view(UsuarioView(Usuario, db.session,
-                   menu_icon_type='fa-solid', menu_icon_value='fa-user'))
-    admin.add_view(RolView(Rol, db.session,
-                   menu_icon_type='fa-solid', menu_icon_value='fa-ruler'))
-    admin.add_view(InsumoView(Insumo, db.session,
-                   menu_icon_type='fa-solid', menu_icon_value='fa-carrot'))
-    admin.add_view(AdminModelView(Proveedor, db.session,
-                   menu_icon_type='fa-solid', menu_icon_value='fa-handshake'))
-    admin.add_view(RecetaView(Receta, db.session,
-                   menu_icon_type='fa-solid', menu_icon_value='fa-clipboard'))
+    admin.add_view(
+        UsuarioView(
+            Usuario, db.session, menu_icon_type="fa-solid", menu_icon_value="fa-user"
+        )
+    )
+    admin.add_view(
+        RolView(Rol, db.session, menu_icon_type="fa-solid", menu_icon_value="fa-ruler")
+    )
+    admin.add_view(
+        InsumoView(
+            Insumo, db.session, menu_icon_type="fa-solid", menu_icon_value="fa-carrot"
+        )
+    )
+    admin.add_view(
+        AdminModelView(
+            Proveedor,
+            db.session,
+            menu_icon_type="fa-solid",
+            menu_icon_value="fa-handshake",
+        )
+    )
+    admin.add_view(
+        RecetaView(
+            Receta,
+            db.session,
+            menu_icon_type="fa-solid",
+            menu_icon_value="fa-clipboard",
+        )
+    )
 
     return admin
--- src/routes/venta.py
+++ src/routes/venta.py
@@ -1,168 +1,237 @@
-from flask import Blueprint, Flask, request,render_template, Response, redirect, url_for
+from flask import (
+    Blueprint,
+    Flask,
+    request,
+    render_template,
+    Response,
+    redirect,
+    url_for,
+)
 from flask_login import login_required, current_user
 
 from flask_wtf.csrf import CSRFProtect
 from flask import g
 from flask import flash, jsonify
 from configu.config import DevelopmentConfig
-from database.models import db, Usuario, Rol, asignacion_rol_usuario, Insumo, Receta, SolicitudProduccion, Venta, DetalleVenta, Compra, Proveedor, LoteGalleta
+from database.models import (
+    db,
+    Usuario,
+    Rol,
+    asignacion_rol_usuario,
+    Insumo,
+    Receta,
+    SolicitudProduccion,
+    Venta,
+    DetalleVenta,
+    Compra,
+    Proveedor,
+    LoteGalleta,
+)
 
 from routes.auth import requires_role
 from forms import forms
 from sqlalchemy.orm import joinedload
-from sqlalchemy import desc, extract,text
+from sqlalchemy import desc, extract, text
 from datetime import datetime, timedelta
 
-venta = Blueprint('venta', __name__, url_prefix="/venta")
+venta = Blueprint("venta", __name__, url_prefix="/venta")
 
 
-@venta.route('/compras')
+@venta.route("/compras")
 @login_required
 def compras():
-    return render_template('modulos/venta/compras.html')
+    return render_template("modulos/venta/compras.html")
 
 
-
-@venta.route('/punto_venta')
+@venta.route("/punto_venta")
 @requires_role("vendedor")
 def punto_venta():
-    return render_template('modulos/venta/punto-venta.html')
+    return render_template("modulos/venta/punto-venta.html")
 
-@venta.route('/solicitud_produccion')
+
+@venta.route("/solicitud_produccion")
 @requires_role("vendedor")
 def solicitud_produccion():
     form = forms.SolicitudProduccionForm(request.form)
-    
+
     # Obtiene todas las recetas de la base de datos
     recetas = Receta.query.all()
-    
+
     # Crea una lista de tuplas con el ID y el nombre de cada receta
     recetas_opciones = [(receta.id, receta.nombre) for receta in recetas]
     recetas_imagenes = [(receta.id, receta.imagen) for receta in recetas]
-    
+
     # Asigna las opciones al campo receta del formulario
     form.receta.choices = recetas_opciones
-    
-    solicitudes = SolicitudProduccion.query.options(
-        joinedload(SolicitudProduccion.receta), # Asume que 'receta' es el nombre de la relación en SolicitudProduccion
-        joinedload(SolicitudProduccion.usuarioSolicitud), # Asume que 'usuarioSolicitud' es el nombre de la relación en SolicitudProduccion
-        joinedload(SolicitudProduccion.usuarioProduccion) # Asume que 'usuarioProduccion' es el nombre de la relación en SolicitudProduccion
-    ).all()
-        
-    return render_template('modulos/venta/solicitudes-produccion.html', form=form, recetas_imagenes=recetas_imagenes, solicitudes=solicitudes)
 
-@venta.route('/solicitud_produccion/nuevo')
+    solicitudes = SolicitudProduccion.query.options(
+        joinedload(
+            SolicitudProduccion.receta
+        ),  # Asume que 'receta' es el nombre de la relación en SolicitudProduccion
+        joinedload(
+            SolicitudProduccion.usuarioSolicitud
+        ),  # Asume que 'usuarioSolicitud' es el nombre de la relación en SolicitudProduccion
+        joinedload(
+            SolicitudProduccion.usuarioProduccion
+        ),  # Asume que 'usuarioProduccion' es el nombre de la relación en SolicitudProduccion
+    ).all()
+
+    return render_template(
+        "modulos/venta/solicitudes-produccion.html",
+        form=form,
+        recetas_imagenes=recetas_imagenes,
+        solicitudes=solicitudes,
+    )
+
+
+@venta.route("/solicitud_produccion/nuevo")
 @requires_role("vendedor")
 def solicitud_produccion_nuevo():
     form = forms.SolicitudProduccionForm(request.form)
-    
+
     # Obtiene todas las recetas de la base de datos
     recetas = Receta.query.all()
-    
+
     # Crea una lista de tuplas con el ID y el nombre de cada receta
     recetas_opciones = [(receta.id, receta.nombre) for receta in recetas]
     recetas_imagenes = [(receta.id, receta.imagen) for receta in recetas]
-    
+
     # Asigna las opciones al campo receta del formulario
     form.receta.choices = recetas_opciones
-    
+
     solicitudes = SolicitudProduccion.query.options(
-        joinedload(SolicitudProduccion.receta), # Asume que 'receta' es el nombre de la relación en SolicitudProduccion
-        joinedload(SolicitudProduccion.usuarioSolicitud), # Asume que 'usuarioSolicitud' es el nombre de la relación en SolicitudProduccion
-        joinedload(SolicitudProduccion.usuarioProduccion) # Asume que 'usuarioProduccion' es el nombre de la relación en SolicitudProduccion
+        joinedload(
+            SolicitudProduccion.receta
+        ),  # Asume que 'receta' es el nombre de la relación en SolicitudProduccion
+        joinedload(
+            SolicitudProduccion.usuarioSolicitud
+        ),  # Asume que 'usuarioSolicitud' es el nombre de la relación en SolicitudProduccion
+        joinedload(
+            SolicitudProduccion.usuarioProduccion
+        ),  # Asume que 'usuarioProduccion' es el nombre de la relación en SolicitudProduccion
     ).all()
 
-        
-    return render_template('modulos/venta/solicitudes-produccion.html', form=form, recetas_imagenes=recetas_imagenes, solicitudes=solicitudes, nuevo=True)
+    return render_template(
+        "modulos/venta/solicitudes-produccion.html",
+        form=form,
+        recetas_imagenes=recetas_imagenes,
+        solicitudes=solicitudes,
+        nuevo=True,
+    )
 
-@venta.route('/solicitud_produccion/nuevo', methods=['POST'])
+
+@venta.route("/solicitud_produccion/nuevo", methods=["POST"])
 @requires_role("vendedor")
 def solicitud_produccion_post():
     form = forms.SolicitudProduccionForm(request.form)
-    
+
     # Obtiene todas las recetas de la base de datos
     recetas = Receta.query.all()
-    
+
     # Crea una lista de tuplas con el ID y el nombre de cada receta
     recetas_opciones = [(receta.id, receta.nombre) for receta in recetas]
     recetas_imagenes = [(receta.id, receta.imagen) for receta in recetas]
-    
+
     # Asigna las opciones al campo receta del formulario
     form.receta.choices = recetas_opciones
-    
-    if form.validate():
-        rol_cocinero = Rol.query.filter_by(nombre='cocinero').first()
 
-        usuario_cocinero=0
+    if form.validate():
+        rol_cocinero = Rol.query.filter_by(nombre="cocinero").first()
+
+        usuario_cocinero = 0
         if rol_cocinero is not None:
             # Luego, filtra los usuarios que tienen el rol 'cocinero' y ordena por 'is_active'
-            usuario_cocinero = Usuario.query.join(asignacion_rol_usuario).join(Rol).filter(Rol.id == rol_cocinero.id).order_by(desc('is_active')).first()
+            usuario_cocinero = (
+                Usuario.query.join(asignacion_rol_usuario)
+                .join(Rol)
+                .filter(Rol.id == rol_cocinero.id)
+                .order_by(desc("is_active"))
+                .first()
+            )
 
             # Si no se encuentra un usuario cocinero con is_active, intenta obtener el usuario con el last_login_at más reciente
             if usuario_cocinero is None:
-                usuario_cocinero = Usuario.query.join(asignacion_rol_usuario).join(Rol).filter(Rol.id == rol_cocinero.id).order_by(desc('last_login_at')).first()
+                usuario_cocinero = (
+                    Usuario.query.join(asignacion_rol_usuario)
+                    .join(Rol)
+                    .filter(Rol.id == rol_cocinero.id)
+                    .order_by(desc("last_login_at"))
+                    .first()
+                )
 
             if usuario_cocinero is not None:
                 usuario_cocinero = usuario_cocinero.id
             else:
-                    # Maneja el caso en que no se encuentre ningún usuario cocinero
+                # Maneja el caso en que no se encuentre ningún usuario cocinero
                 usuario_cocinero = current_user.id
-        
+
         solicitud = SolicitudProduccion(
             idReceta=form.receta.data,
             idUsuarioSolicitud=current_user.id,
             idUsuarioProduccion=usuario_cocinero,
             fecha_solicitud=form.fecha_solicitud.data,
             tandas=form.tandas.data,
-            status=1
+            status=1,
         )
-        
+
         db.session.add(solicitud)
         db.session.commit()
-        
-        flash('Solicitud de producción creada correctamente', 'success')
-    
-    
-    solicitudes = SolicitudProduccion.query.options(
-        joinedload(SolicitudProduccion.receta), # Asume que 'receta' es el nombre de la relación en SolicitudProduccion
-        joinedload(SolicitudProduccion.usuarioSolicitud), # Asume que 'usuarioSolicitud' es el nombre de la relación en SolicitudProduccion
-        joinedload(SolicitudProduccion.usuarioProduccion) # Asume que 'usuarioProduccion' es el nombre de la relación en SolicitudProduccion
-    ).all()
-    
-    return redirect(url_for('venta.solicitud_produccion'))
 
-@venta.route('/solicitud_produccion/edit/<int:id>', methods=['GET','POST'])
+        flash("Solicitud de producción creada correctamente", "success")
+
+    solicitudes = SolicitudProduccion.query.options(
+        joinedload(
+            SolicitudProduccion.receta
+        ),  # Asume que 'receta' es el nombre de la relación en SolicitudProduccion
+        joinedload(
+            SolicitudProduccion.usuarioSolicitud
+        ),  # Asume que 'usuarioSolicitud' es el nombre de la relación en SolicitudProduccion
+        joinedload(
+            SolicitudProduccion.usuarioProduccion
+        ),  # Asume que 'usuarioProduccion' es el nombre de la relación en SolicitudProduccion
+    ).all()
+
+    return redirect(url_for("venta.solicitud_produccion"))
+
+
+@venta.route("/solicitud_produccion/edit/<int:id>", methods=["GET", "POST"])
 @requires_role("vendedor")
 def edit_solicitud_produccion(id):
     form2 = forms.ModificarSolicitudProduccionForm(request.form)
     form = forms.SolicitudProduccionForm(request.form)
-    
-    if request.method == 'POST' and form2.validate():
-        
+
+    if request.method == "POST" and form2.validate():
+
         solicitud = SolicitudProduccion.query.get(id)
         solicitud.tandas = form.tandas.data
         solicitud.fecha_solicitud = form.fecha_solicitud.data
         solicitud.mensaje = form2.mensaje.data
         db.session.commit()
-        flash('Solicitud de producción modificada correctamente', 'success')
-        return redirect(url_for('venta.solicitud_produccion'))
-        
+        flash("Solicitud de producción modificada correctamente", "success")
+        return redirect(url_for("venta.solicitud_produccion"))
+
     # Obtiene todas las recetas de la base de datos
     recetas = Receta.query.all()
-    
+
     # Crea una lista de tuplas con el ID y el nombre de cada receta
     recetas_opciones = [(receta.id, receta.nombre) for receta in recetas]
     recetas_imagenes = [(receta.id, receta.imagen) for receta in recetas]
-    
+
     # Asigna las opciones al campo receta del formulario
     form.receta.choices = recetas_opciones
-    
+
     solicitudes = SolicitudProduccion.query.options(
-        joinedload(SolicitudProduccion.receta), # Asume que 'receta' es el nombre de la relación en SolicitudProduccion
-        joinedload(SolicitudProduccion.usuarioSolicitud), # Asume que 'usuarioSolicitud' es el nombre de la relación en SolicitudProduccion
-        joinedload(SolicitudProduccion.usuarioProduccion) # Asume que 'usuarioProduccion' es el nombre de la relación en SolicitudProduccion
+        joinedload(
+            SolicitudProduccion.receta
+        ),  # Asume que 'receta' es el nombre de la relación en SolicitudProduccion
+        joinedload(
+            SolicitudProduccion.usuarioSolicitud
+        ),  # Asume que 'usuarioSolicitud' es el nombre de la relación en SolicitudProduccion
+        joinedload(
+            SolicitudProduccion.usuarioProduccion
+        ),  # Asume que 'usuarioProduccion' es el nombre de la relación en SolicitudProduccion
     ).all()
-    
+
     # Busca la solicitud de producción específica por su ID
     solicitud_especifica = SolicitudProduccion.query.get(id)
 
@@ -172,61 +241,89 @@ def edit_solicitud_produccion(id):
     if solicitud_especifica:
         nombre_receta = solicitud_especifica.receta.nombre
         imagen_receta = solicitud_especifica.receta.imagen
-    
-    return render_template('modulos/venta/solicitudes-produccion.html', form=form, form2=form2, recetas_imagenes=recetas_imagenes, solicitudes=solicitudes, editar=True,nombre_receta=nombre_receta,imagen_receta = imagen_receta, id_receta=id)
 
-@venta.route('/solicitud_produccion/delete', methods=['POST'])
+    return render_template(
+        "modulos/venta/solicitudes-produccion.html",
+        form=form,
+        form2=form2,
+        recetas_imagenes=recetas_imagenes,
+        solicitudes=solicitudes,
+        editar=True,
+        nombre_receta=nombre_receta,
+        imagen_receta=imagen_receta,
+        id_receta=id,
+    )
+
+
+@venta.route("/solicitud_produccion/delete", methods=["POST"])
 @requires_role("vendedor")
 def delete_solicitud_produccion():
-    id = request.form.get('id')
+    id = request.form.get("id")
     solicitud = SolicitudProduccion.query.get_or_404(id)
     db.session.delete(solicitud)
     db.session.commit()
-    flash('Solicitud de producción eliminada correctamente', 'success')
-    return redirect(url_for('venta.solicitud_produccion'))
+    flash("Solicitud de producción eliminada correctamente", "success")
+    return redirect(url_for("venta.solicitud_produccion"))
 
 
-@venta.route('/almacen/galletas', methods=['GET', 'POST'])
+@venta.route("/almacen/galletas", methods=["GET", "POST"])
 @requires_role("vendedor")
 def lotes_galletas():
     form = forms.BusquedaLoteGalletaForm(request.form)
-    
-    form.receta.choices = [(0, 'Todas las recetas')] + [(receta.id, receta.nombre) for receta in Receta.query.all()]
-    
-    if request.method=="POST" and form.validate():
+
+    form.receta.choices = [(0, "Todas las recetas")] + [
+        (receta.id, receta.nombre) for receta in Receta.query.all()
+    ]
+
+    if request.method == "POST" and form.validate():
         fecha_inicio = form.fecha_inicio.data
         fecha_fin = form.fecha_fin.data
         receta = form.receta.data
         lotes = []
-        
-        base_query = db.session.query(LoteGalleta, Receta.nombre, Usuario.nombre).join(Receta, LoteGalleta.idReceta == Receta.id).join(Usuario, LoteGalleta.idUsuarios == Usuario.id)
+
+        base_query = (
+            db.session.query(LoteGalleta, Receta.nombre, Usuario.nombre)
+            .join(Receta, LoteGalleta.idReceta == Receta.id)
+            .join(Usuario, LoteGalleta.idUsuarios == Usuario.id)
+        )
         print(fecha_inicio, fecha_fin, receta)
         if receta == 0:
             lotes = base_query.filter(
                 LoteGalleta.fecha_entrada >= fecha_inicio,
                 LoteGalleta.fecha_entrada <= fecha_fin,
-                LoteGalleta.cantidad > 0
+                LoteGalleta.cantidad > 0,
             ).all()
         else:
             lotes = base_query.filter(
                 LoteGalleta.fecha_entrada >= fecha_inicio,
                 LoteGalleta.fecha_entrada <= fecha_fin,
                 LoteGalleta.idReceta == receta,
-                LoteGalleta.cantidad > 0
+                LoteGalleta.cantidad > 0,
             ).all()
-        
-        return render_template('modulos/venta/galletas.html', form=form, lotes=lotes, lista=True)
-    
-    lotes = db.session.query(LoteGalleta, Receta.nombre, Usuario.nombre).join(Receta, LoteGalleta.idReceta == Receta.id).join(Usuario, LoteGalleta.idUsuarios == Usuario.id).filter(LoteGalleta.cantidad > 0).all()
-    
-    return render_template('modulos/venta/galletas.html', form=form, lotes=lotes, lista = True)
 
-@venta.route('/merma/galletas', methods=['GET','POST'])
+        return render_template(
+            "modulos/venta/galletas.html", form=form, lotes=lotes, lista=True
+        )
+
+    lotes = (
+        db.session.query(LoteGalleta, Receta.nombre, Usuario.nombre)
+        .join(Receta, LoteGalleta.idReceta == Receta.id)
+        .join(Usuario, LoteGalleta.idUsuarios == Usuario.id)
+        .filter(LoteGalleta.cantidad > 0)
+        .all()
+    )
+
+    return render_template(
+        "modulos/venta/galletas.html", form=form, lotes=lotes, lista=True
+    )
+
+
+@venta.route("/merma/galletas", methods=["GET", "POST"])
 @requires_role("vendedor")
 def merma_galletas():
     form = forms.MermaGalletaForm(request.form)
     if form.validate():
         tipo_medida = form.tipo_medida.data
         cantidad = form.cantidad.data
-        
-    return redirect(url_for('venta.lotes_galletas'))
+
+    return redirect(url_for("venta.lotes_galletas"))
